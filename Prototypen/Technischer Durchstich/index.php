89a2560bb2489f4d62ea5bd184e951047cc1268e
tim-peters <tim_peters@gmx.de>
2015-01-09 16:38:00 +0100

    clean up & comments

--- Prototypen/Technischer Durchstich/index.php
+++ Prototypen/Technischer Durchstich/index.php
@@ -13,7 +13,7 @@ include_once("./display_functions.inc");
 $user_objects = array(); // create an empty array named $user_objects
 if($user_db_content = $GLOBALS['db']->query("SELECT id FROM users")) // get all User with their id from database
 while($row = $user_db_content->fetch_object()) { // for each user...
-	$user_objects[] = new User($row->id); // create an object from class User and store it in the $user_objects array
+	$user_objects[$row->id] = new User($row->id); // create an object from class User and store it in the $user_objects array
 }
 $user_db_content->close();
 
@@ -21,11 +21,20 @@ $user_db_content->close();
 $state = $_GET['state'];
 $id = $_GET['id'];
 
-$actual_user = 2; // FIXME: Should be defined in an other way (e.g. cookie);
+// Set the user which is actually viewing/acting
+if($_POST['change_user']) // if a new user is getting set
+{
+	setcookie("user",$_POST['user']); // set a cookie to "remember" this choice
+	$actual_user = $_POST['user'];
+}
+elseif(isset($_COOKIE['user'])) // if a user has been set before
+	$actual_user = $_COOKIE['user'];
+else // fallback
+	$actual_user = 2;
 
 
 ?>
-<!DOCTYPE html ng-app>
+<!DOCTYPE html>
 <html>
 <head>
 	<title></title>
@@ -34,11 +43,33 @@ $actual_user = 2; // FIXME: Should be defined in an other way (e.g. cookie);
 
 </head>
 <body>
+<div class="free" style="width:500px;margin:0 auto;">
+	<form action="" method="POST">
+		Act/View as 
+		<select name="user" size="1">
+			<?php
+			if($user_db_content = $GLOBALS['db']->query("SELECT id, name FROM users")) 
+			while($row = $user_db_content->fetch_object()) 
+				echo "			<option value='".$row->id."'>".$row->name."</option>\n"; 
+			?>
+		</select>
+		<input type="submit" name="change_user" />
+	</form>
+</div>
 <section id="mirror">
 	<div class="mirror">
 <?php
 	//echo showUserbar($user_objects, $actual_user);
+	echo "View of ".$user_objects[$actual_user]->name;
 	switch($state) {
+		case "sign_up":
+		break;
+		case "start":
+			echo showUserbar($user_objects, $actual_user);
+		break;
+		case "add_moment":
+			echo showUserbar($user_objects, $actual_user, $id);
+		break;
 		case "feelings":
 		break;
 		case "good_moment":
@@ -47,24 +78,17 @@ $actual_user = 2; // FIXME: Should be defined in an other way (e.g. cookie);
 		break;
 		case "lueckentext":
 		break;
-		case "moments":
-		break;
 		case "scream":
 		break;
-		case "sign_up":
-		break;
-		case "start":
-			echo showUserbar($user_objects, $actual_user);
-		break;
 		default:
-			echo "<img src='./img/mirror_states/welcome.jpg'>";
+			echo "<a href='?state=start'><img src='./img/mirror_states/welcome.jpg'></a>";
 	}
 ?>
 	</div>
 </section>
 <section id="bracelet">
 	<div class="bracelet vibration">
-		<br>User 1
+		<br><?php echo $user_objects[$actual_user]->name; ?>
 		<span class="LED blink_green"></span>
 	</div>
 	<div class="bracelet">
